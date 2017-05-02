b5de1648ddedb6b453b3c76672761c7b23ce52fb
Ben Zörb <ben@sommerlaune.com>
2017-05-02 14:50:03 +0200

    downgrade jsdom

--- test/helpers/setup-browser-env.js
+++ test/helpers/setup-browser-env.js
@@ -2,18 +2,15 @@
 /**
  * Created by ben on 06.04.16.
  */
-require('babel-register'); // eslint-disable-line import/no-unassigned-import
 const fs = require('fs');
 const path = require('path');
 const jsdom = require('jsdom');
 
-const {JSDOM} = jsdom;
 const css = fs.readFileSync(path.join(__dirname, '../fixtures/bootstrap.css'), 'utf8');
 const html = fs.readFileSync(path.join(__dirname, '../fixtures/test.html'), 'utf8');
-const dom = new JSDOM(html);
 
-global.window = dom.window;
-global.document = dom.window.document;
+global.document = jsdom.jsdom(html);
+global.window = document.defaultView;
 
 // Append css
 const head = global.document.getElementsByTagName('head')[0];
