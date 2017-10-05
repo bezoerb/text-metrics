63b882815f6cc50b9c002c88cc38190ae4fc7493
Ben Zörb <ben@sommerlaune.com>
2017-10-05 11:48:56 +0200

    Fixes test after jsdom update

--- test/helpers/setup-browser-env.js
+++ test/helpers/setup-browser-env.js
@@ -4,13 +4,14 @@
  */
 const fs = require('fs');
 const path = require('path');
-const jsdom = require('jsdom');
+const {JSDOM} = require('jsdom');
 
 const css = fs.readFileSync(path.join(__dirname, '../fixtures/bootstrap.css'), 'utf8');
 const html = fs.readFileSync(path.join(__dirname, '../fixtures/test.html'), 'utf8');
 
-global.document = jsdom.jsdom(html);
-global.window = document.defaultView;
+const dom = new JSDOM(html);
+global.window = dom.window;
+global.document = global.window.document;
 
 // Append css
 const head = global.document.getElementsByTagName('head')[0];
